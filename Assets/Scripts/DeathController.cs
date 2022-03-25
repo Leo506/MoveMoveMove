003be84b0ccc29a455a47ba734e94c4fc7bbbf42
Artem <leoartemlev@yandex.ru>
2022-03-25 17:46:06 +0500

    Добавил скрипт, следящий сколько времени игрок простоял

---
+++ Assets/Scripts/DeathController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathController : MonoBehaviour
+{
+    [SerializeField] int secBeforeDie;
+
+    public static event System.Action PlayerDiedEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CharacterMovement.StopRunEvent += StartCountDown;
+        CharacterMovement.StartRunEvent += StopCountDown;
+    }
+
+    private void OnDestroy()
+    {
+        CharacterMovement.StopRunEvent -= StartCountDown;
+        CharacterMovement.StartRunEvent -= StopCountDown;
+    }
+
+    private void StartCountDown()
+    {
+        StartCoroutine(CountDown(secBeforeDie));
+    }
+
+    private void StopCountDown()
+    {
+        StopAllCoroutines();
+    }
+
+    IEnumerator CountDown(int sec)
+    {
+        yield return new WaitForSeconds(sec);
+
+        Debug.Log("Игрок умер");
+        PlayerDiedEvent?.Invoke();
+    }
+}
