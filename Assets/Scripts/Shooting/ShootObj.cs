f19e0445aeecf6069d9238bbe462ad1ccb32102a
Artem <leoartemlev@yandex.ru>
2022-03-25 17:59:21 +0500

    Добавил время жизни для пули

--- Assets/Scripts/Shooting/ShootObj.cs
+++ Assets/Scripts/Shooting/ShootObj.cs
@@ -6,12 +6,16 @@ using UnityEngine;
 public class ShootObj : MonoBehaviour
 {
     [SerializeField] float shootSpeed;
+    [SerializeField] float lifeTime;
     [SerializeField] ParticleSystem destroyEffect;
 
-    Vector3 movementDir = Vector3.zero;
-    Rigidbody rb;
+    private Vector3 movementDir = Vector3.zero;
+    
+    private Rigidbody rb;
 
     private List<string> ignoreObjTags = new List<string>() { "Player" };
+
+    private float bornTime;
 
     public void SetIgnoreList(List<string> ignoreList)
     {
@@ -22,11 +26,21 @@ public class ShootObj : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         movementDir = dir.normalized;
+        bornTime = Time.time;
     }
 
     private void FixedUpdate()
     {
         rb.velocity = movementDir * shootSpeed * Time.fixedDeltaTime;
+
+        if (Time.time - bornTime >= lifeTime)
+            DestroyShootObj();
+    }
+
+    private void DestroyShootObj()
+    {
+        Instantiate(destroyEffect, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -34,7 +48,6 @@ public class ShootObj : MonoBehaviour
         if (ignoreObjTags.Contains(collision.collider.tag))
             return;
 
-        Instantiate(destroyEffect, transform.position, Quaternion.identity);
-        Destroy(this.gameObject);
+        DestroyShootObj();
     }
 }
