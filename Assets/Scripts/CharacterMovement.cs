09a199986ddaffd7b015233aac595a053254b025
Artem <leoartemlev@yandex.ru>
2022-03-25 15:02:23 +0500

    Старт работы

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMovement : MonoBehaviour
+{
+    [Header("Скорость движения игрока")]
+    [SerializeField] float characterSpeed;
+
+    CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (movement != Vector3.zero)
+        {
+            characterController.Move(movement * Time.deltaTime * characterSpeed);
+            transform.rotation = Quaternion.LookRotation(movement);
+        }
+    }
+}
