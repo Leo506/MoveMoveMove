ac9e217a3c318445d77702bb90dbf2637e66d3ab
Artem <leoartemlev@yandex.ru>
2022-03-25 16:58:24 +0500

    Добавил врага, который преследует игрока

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(PlayerData.Instance.gameObject.transform.position);
+    }
+
+}
---
+++ Assets/Scripts/LvlMap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshSurface))]
+public class LvlMap : MonoBehaviour
+{
+    NavMeshSurface surface;
+
+    void Start()
+    {
+        surface = GetComponent<NavMeshSurface>();
+        surface.BuildNavMesh();
+    }
+
+}
